a47697edd92a39445ec153cf800ae0944f63d230
Russ Adams <russ.adams@gmail.com>
2014-02-08 14:15:18 -0500

    Removing unnecessary functions from LevelLoader.

--- src/LevelLoader.hpp
+++ src/LevelLoader.hpp
@@ -6,12 +6,8 @@
 class LevelLoader {
 
 public:
-    static Level* Load(int level);
     static uint16_t GetTotal();
 
-private:
-    static bool ParseLine(Level* level, int& parseX, int& parseY, std::string line);
-    static void ParseVar(Level* level, std::string line);
 };
 
 #endif//LEVELLOADER_HPP
