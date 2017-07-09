2db95e63ab0e14d3db9cc703cbbca1164bacb829
Russ Adams <russ.adams@gmail.com>
2017-07-08 22:22:51 -0400

    Nicer Conveyor Animations
    This fixes the conveyor animations following the conveyor refactor last
    month. They look more fluid and a little high contrast now.

--- src/view/AnimationFactory.cpp
+++ src/view/AnimationFactory.cpp
@@ -120,28 +120,24 @@ std::vector<uint16_t> AnimationFactory::getFrameList(AnimationType animationType
     switch (animationType) {
 
         case ANIMATION_TYPE_CONVEY_DOWN:
-            FL(1,0)FL(1,1)FL(1,2)FL(1,3)
-                FL(1,4)FL(1,5)FL(1,6)FL(1,7)
-                FL(1,8)FL(1,9)FL(1,10)FL(1,11)
-                break;
+			FL(1,0)FL(1,1)FL(1,2)FL(1,3)
+				FL(1,4)FL(1,5)FL(1,6)
+				break;
 
         case ANIMATION_TYPE_CONVEY_UP:
-            FL(2,0)FL(2,1)FL(2,2)FL(2,3)
-                FL(2,4)FL(2,5)FL(2,6)FL(2,7)
-                FL(2,8)FL(2,9)FL(2,10)FL(2,11)
-                break;
+			FL(3,0)FL(3,1)FL(3,2)FL(3,3)
+				FL(3,4)FL(3,5)FL(3,6)
+				break;
 
         case ANIMATION_TYPE_CONVEY_LEFT:
-            FL(0,0)FL(0,1)FL(0,2)FL(0,3)
-                FL(0,4)FL(0,5)FL(0,6)FL(0,7)
-                FL(0,8)FL(0,9)FL(0,10)FL(0,11)
-                break;
+			FL(0,0)FL(0,1)FL(0,2)FL(0,3)
+				FL(0,4)FL(0,5)FL(0,6)
+				break;
 
         case ANIMATION_TYPE_CONVEY_RIGHT:
-            FL(3,0)FL(3,1)FL(3,2)FL(3,3)
-                FL(3,4)FL(3,5)FL(3,6)FL(3,7)
-                FL(3,8)FL(3,9)FL(3,10)FL(3,11)
-                break;
+			FL(2,0)FL(2,1)FL(2,2)FL(2,3)
+				FL(2,4)FL(2,5)FL(2,6)
+				break;
 
         case ANIMATION_TYPE_EMPTY:
         case ANIMATION_TYPE_WALL:
